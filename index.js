f2f4f89e367c32299bfc0679f970e3e0d97a5ad2
Eric Uberman <euberman@gmail.com>
2020-09-08 22:38:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   let firstArray = [];
-  for (let i = 0; i < 9; i++) {
-    str = str + i;
+  for (let i = 0; i < musicians.length; i++) {
+    str = ;
     
   }
 }
