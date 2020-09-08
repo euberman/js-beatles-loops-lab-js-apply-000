b01430fcbdaea406ae3bb622ca371611198724e5
Eric Uberman <euberman@gmail.com>
2020-09-08 22:37:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   let firstArray = [];
-  for ()
+  for (let i = 0; i < 9; i++) {
+    str = str + i;
+    
+  }
 }
 
 function johnLennonFacts(){
