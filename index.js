36661b0aff20bf935be001760acf3f9f104c9792
Eric Uberman <euberman@gmail.com>
2020-09-08 22:52:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-  let firstArray = [],str='';
+  let firstArray = [], str='';
   for (let i = 0; i < musicians.length; i++) {
     str = musicians[i] + " plays " + instruments[i];
     firstArray.push(str);
@@ -12,9 +12,12 @@ function johnLennonFacts(facts){
   let str = '', i = 0, factList = [];
   while (i < facts.length) {
     str = facts[i] + "!!!";
+    factList.push(str);
   }
+  return factList;
 }
 
-function iLoveTheBeatles(){
+function iLoveTheBeatles(n){
+  let a = [];
   
 }
